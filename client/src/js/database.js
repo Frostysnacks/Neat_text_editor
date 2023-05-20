53d207bcb7c77609604086c9b3bdee2addb9ee04
Frostysnacks <rbernier93@gmail.com>
2023-05-20 16:18:52 -0400

    pushing to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.put(content)
+  const request = store.put({ id: 1, value: content})
 
   const result = await request;
   console.log('result.value', result);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -26,5 +26,6 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+   //clears the prompt
     window.deferredPrompt = null;
 });
